ffaad78230db08f79863459cc54671fd7195604e
Shrey113 <shreyp518@gmail.com>
2024-10-22 23:28:15 +0530

    sss

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://node-test-jafv-eea70sv8a-shrey113s-projects.vercel.app/',
   }
 });
 
